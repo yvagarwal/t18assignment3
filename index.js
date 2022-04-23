b4f73ae64331fa0d669d67242507d151b02355c6
Yash Vardhan Agarwal <83070042+yvagarwal@users.noreply.github.com>
2022-04-23 18:55:46 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 
+const { Router } = require('express');
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -6,10 +7,8 @@ const port = 3000;
 app.listen(3000, () =>{
     console.log('App is listening on port 3000');
 })
-const mongoose = require('mongoose');
-const student = require('./models/Students');
-const { getMaxListeners } = require('./models/Students');
-mongoose.connect('mongodb://localhost:27017/school');
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/school', { useNewUrlParser: true });
 
 const students = require('./models/Students');
 const teachers = require('./models/Teachers');
@@ -172,3 +171,159 @@ async function getAssignedStudentsToTeacher(teacherId){
         console.log(data);
     })
 }
+
+//routes
+//const bodyParser = require('body-parser');
+app.use(express.json());
+//app.use(bodyParser.json());
+
+app.get('/students', async (req,res)=>{    //to view all students
+    try{
+        const sts = await students.find()
+        res.json(sts);
+    }
+    catch (err){
+        res.json({message: err.message});
+    }
+})
+
+app.get('/student/:id', getStudent, (req,res) =>{   //to get one student using its id
+    res.json(res.stu)
+})
+
+app.get('/teachers', async (req,res)=>{    //to view all teachers
+    try{
+        const sts = await teachers.find()
+        res.json(sts);
+    }
+    catch (err){
+        res.json({message: err.message});
+    }
+})
+
+app.get('/teacher/:id', getTeacher, (req,res) =>{   //to get one teacher using its id
+    res.json(res.tea)
+})
+
+app.post('/addTeacher',async (req,res) => {   //adding a teacher
+    const tea = new teachers({
+        name: req.body.name,
+        email: req.body.email,
+        subject: req.body.subject
+    });
+    try{
+        const newteacher = await tea.save()
+        res.status(201).json(newteacher)
+
+    }catch(err){
+        res.status(400).json({message: err.message})
+    }
+});
+
+app.post('/addStudent',async (req,res) => {   //adding a student
+    const stu = new students({
+        name: req.body.name,
+        email: req.body.email,
+        class: req.body.class,
+        section: req.body.section,
+        assignTeacher: req.body.assignedTeacher 
+    });
+    try{
+        const newstudent = await stu.save()
+        res.status(201).json(newstudent)
+
+    }catch(err){
+        res.status(400).json({message: err.message})
+    }
+});
+
+app.patch('/student/:id', getStudent, async (req, res) => {   //updating a student
+    if (req.body.name != null) {
+      res.stu.name = req.body.name
+    }
+    if (req.body.email != null) {
+      res.stu.email = req.body.email
+    }
+    if (req.body.class != null) {
+        res.stu.class = req.body.class
+      }
+    if (req.body.section != null) {
+        res.stu.section = req.body.section
+      }
+      if (req.body.assignedTeacher != null) {
+        res.stu.assignedTeacher = req.body.assignedTeacher
+      }
+    
+    try {
+      const updatedStudent = await res.stu.save()
+      res.json(updatedStudent)
+    } catch (err) {
+      res.status(400).json({ message: err.message })
+    }
+  })
+
+  app.patch('/student/:id', getStudent, async (req, res) => {   //updating a teacher
+    if (req.body.name != null) {
+      res.tea.name = req.body.name
+    }
+    if (req.body.email != null) {
+      res.tea.email = req.body.email
+    }
+    if (req.body.subject != null) {
+        res.tea.subject = req.body.subject
+      }
+    
+    try {
+      const updatedTeacher = await res.tea.save()
+      res.json(updatedTeacher)
+    } catch (err) {
+      res.status(400).json({ message: err.message })
+    }
+  })
+  
+
+  app.delete('/student/:id', getStudent, async (req, res) => {  //delete a student
+    try {
+      await res.stu.remove()
+      res.json({ message: 'Deleted Student' })
+    } catch (err) {
+      res.status(500).json({ message: err.message })
+    }
+  })
+
+  app.delete('/teacher/:id', getTeacher, async (req, res) => {  //delete a teacher
+    try {
+      await res.tea.remove()
+      res.json({ message: 'Deleted Teacher' })
+    } catch (err) {
+      res.status(500).json({ message: err.message })
+    }
+  })
+
+async function getStudent(req, res, next) {   //middleware for student
+    let stu
+    try {
+      stu = await students.findById(req.params.id)
+      if (stu == null) {
+        return res.status(404).json({ message: 'Cannot find student' })
+      }
+    } catch (err) {
+      return res.status(500).json({ message: err.message })
+    }
+    res.stu = stu
+    next()
+  }
+
+    async function getTeacher(req, res, next) {  //middleware for teacher
+    let tea
+    try {
+      tea = await students.findById(req.params.id)
+      if (tea == null) {
+        return res.status(404).json({ message: 'Cannot find teacher' })
+      }
+    } catch (err) {
+      return res.status(500).json({ message: err.message })
+    }
+    res.tea = tea
+    next()
+  }
